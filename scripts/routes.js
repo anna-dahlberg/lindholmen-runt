9f052585a3f39c38070230390571e5de868726e3
anna-dahlberg <anna.dahlberg@live.se>
2025-08-19 14:19:40 +0200

    structrued files with folders

---
+++ scripts/routes.js
@@ -0,0 +1,127 @@
+//Route 1: ~10 min
+export const route_1 = [
+    {
+	id: 1,
+	name: "Dips mot bänkarna",
+	lat: 57.42220,
+	lng: 11.56092,
+	easy_challenge: "Gör 3x3 dips",
+	medium_challenge: "Gör 6x3 dips",
+	hard_challenge: "Gör 12x3 dips"
+},
+{
+	id: 2,
+	name: "Squats vid havet.",
+	lat: 57.42198,
+	lng: 11.56189,
+	easy_challenge: "Gör 3x3 squats.",
+	medium_challenge: "6x3 squats",
+	hard_challenge: "12x3 squats"
+},
+{
+	id: 3,
+	name: "Idioten mellan bågarna",
+	lat: 57.42182,
+	lng: 11.56185,
+	easy_challenge: "Spring mellan bågarna 3x3",
+	medium_challenge: "Spring mellan bågarna 6x3",
+	hard_challenge: "Spring mellan bågarna 12x3"
+},
+{
+	id: 4,
+	name: "Utfallsteg på bryggan",
+	lat: 57.42159,
+	lng: 11.56157,
+	easy_challenge: "Gör 3x3 utfallssteg",
+	medium_challenge: "Gör 6x3 utfallssteg",
+	hard_challenge: "Gör 12x3 utfallssteg"
+}
+]
+
+export const route_2 = [
+    {
+	id: 1,
+	name: "Dips mot bänkarna",
+	lat: 57.42220,
+	lng: 11.56092,
+	easy_challenge: "Gör 3x3 dips",
+	medium_challenge: "Gör 6x3 dips",
+	hard_challenge: "Gör 12x3 dips"
+},
+{
+	id: 2,
+	name: "Squats vid havet.",
+	lat: 57.42198,
+	lng: 11.56189,
+	easy_challenge: "Gör 3x3 squats.",
+	medium_challenge: "6x3 squats",
+	hard_challenge: "12x3 squats"
+},
+{
+	id: 3,
+	name: "Idioten i bågen",
+	lat: 57.42182,
+	lng: 11.56185,
+	easy_challenge: "Spring mellan bågarna 3x3",
+	medium_challenge: "Spring mellan bågarna 6x3",
+	hard_challenge: "Spring mellan bågarna 12x3"
+},
+{
+	id: 4,
+	name: "Utfallsteg på bryggan",
+	lat: 57.42159,
+	lng: 11.56157,
+	easy_challenge: "Spring mellan bågarna 3x3",
+	medium_challenge: "Spring mellan bågarna 6x3",
+	hard_challenge: "Spring mellan bågarna 12x3"
+},
+{
+	id: 5,
+	name: "Monkey bars",
+	lat: 57.42187,
+	lng: 11.56049,
+	easy_challenge: "Gör 3x3 pull ups",
+	medium_challenge: "Gör 6x3 pull ups",
+	hard_challenge: "Gör 12x3 pull ups"
+},
+{
+	id: 6,
+	name: "Situps på bänk",
+	lat: 57.42187,
+	lng: 11.56040,
+	easy_challenge: "Gör 3x3 situps",
+	medium_challenge: "Gör 6x3 situps",
+	hard_challenge: "Gör 12x3 situps"
+},
+{
+	id: 7,
+	name: "Trappsprint",
+	lat: 57.42192,
+	lng: 11.56028,
+	easy_challenge: "Spring upp för trappan 1x1",
+	medium_challenge: "Spring upp och ner 1x2",
+	hard_challenge: "Spring upp och ner 1x3"
+},
+{
+	id: 8,
+	name: "Upp för liten backe",
+	lat: 57.42201,
+	lng: 11.55599,
+	easy_challenge: "Gå",
+	medium_challenge: "Jogga",
+	hard_challenge: "Spring"
+},
+{
+	id: 9,
+	name: "Tåhävning",
+	lat: 57.42242,
+	lng: 11.56011,
+	easy_challenge: "Tåhävningar på två ben 12x3",
+	medium_challenge: "Tåhävningar på 1 ben 12x3",
+	hard_challenge: "Tåhävningar mot trottoaren 12x3"
+}
+]
+
+export const route_3 = [
+
+]
