07b07cc0d60d3d485527ab3d51ec1fbdd3be5a68
anna-dahlberg <anna.dahlberg@live.se>
2025-08-19 14:21:17 +0200

    added all routes

--- scripts/routes.js
+++ scripts/routes.js
@@ -123,5 +123,123 @@ export const route_2 = [
 ]
 
 export const route_3 = [
+	{
+	id: 1,
+	name: "Dips mot bänkarna",
+	lat: 57.42220,
+	lng: 11.56092,
+	easy_challenge: "Gör 3x3 dips",
+	medium_challenge: "Gör 6x3 dips",
+	hard_challenge: "Gör 12x3 dips"
+},
+{
+	id: 2,
+	name: "Squats vid havet.",
+	lat: 57.42198,
+	lng: 11.56189,
+	easy_challenge: "Gör 3x3 squats.",
+	medium_challenge: "6x3 squats",
+	hard_challenge: "12x3 squats"
 
+
+},
+{
+	id: 3,
+	name: "Idioten i bågen",
+	lat: 57.42182,
+	lng: 11.56185,
+	easy_challenge: "Spring mellan bågarna 3x3",
+	medium_challenge: "Spring mellan bågarna 6x3",
+	hard_challenge: "Spring mellan bågarna 12x3"
+},
+{
+	id: 4,
+	name: "Utfallsteg på bryggan",
+	lat: 57.42159,
+	lng: 11.56157,
+	easy_challenge: "Spring mellan bågarna 3x3",
+	medium_challenge: "Spring mellan bågarna 6x3",
+	hard_challenge: "Spring mellan bågarna 12x3"
+},
+{
+	id: 5,
+	name: "Monkey bars",
+	lat: 57.42187,
+	lng: 11.56049,
+	easy_challenge: "Gör 3x3 pull ups",
+	medium_challenge: "Gör 6x3 pull ups",
+	hard_challenge: "Gör 12x3 pull ups"
+},
+{
+	id: 6,
+	name: "Situps på bänk",
+	lat: 57.42187,
+	lng: 11.56040,
+	easy_challenge: "Gör 3x3 situps",
+	medium_challenge: "Gör 6x3 situps",
+	hard_challenge: "Gör 12x3 situps"
+},
+{
+	id: 7,
+	name: "Trappsprint",
+	lat: 57.42192,
+	lng: 11.56028,
+	easy_challenge: "Spring upp för trappan 1x1",
+	medium_challenge: "Spring upp och ner 1x2",
+	hard_challenge: "Spring upp och ner 1x3"
+},
+{
+	id: 8,
+	name: "Upp för liten backe",
+	lat: 57.42201,
+	lng: 11.55599,
+	easy_challenge: "Gå",
+	medium_challenge: "Jogga",
+	hard_challenge: "Spring"
+},
+{
+	id: 9,
+	name: "Tåhävning",
+	lat: 57.42242,
+	lng: 11.56011,
+	easy_challenge: "Tåhävningar på två ben 12x3",
+	medium_challenge: "Tåhävningar på 1 ben 12x3",
+	hard_challenge: "Tåhävningar mot trottoaren 12x3"
+},
+{
+	id: 1,
+	name: "Dips mot bänkarna ",
+	lat: 57.42220,
+	lng: 11.56092,
+	easy_challenge: "Gör 3x3 dips",
+	medium_challenge: "Gör 6x3 dips",
+	hard_challenge: "Gör 12x3 dips"
+},
+{
+	id: 2,
+	name: "Plankan i gräset",
+	lat: 57.42255,
+	lng: 11.56061,
+	easy_challenge: "Gör plankan 15 sek 1x3",
+	medium_challenge: "Gör plankan 30 sek 1x3",
+	hard_challenge: "Gör plankan 30 sek 1x6"
+},
+{
+	id: 3,
+	name: "Idioten i trappan",
+	lat: 57.42261,
+	lng: 11.56032,
+	easy_challenge: "Jogga ner för trappan",
+	medium_challenge: "Spring ner för trappan",
+	hard_challenge: "Spring upp och ner 1x1"
+},
+{
+	id: 4,
+	name: "Tåhävning",
+	lat: 57.42242,
+	lng: 11.56011,
+	easy_challenge: "Tåhävningar på två ben 12x3",
+	medium_challenge: "Tåhävningar på 1 ben 12x3",
+	hard_challenge: "Tåhävningar mot trottoaren 12x3"
+}
 ]
